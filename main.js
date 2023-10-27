02222f73b1c66c47ff181c0b16b38426c4badf4c
codeFabFit <fabgar14@gmail.com>
2023-10-27 14:56:42 -0400

    making sure set up is good and detailed javascript with duties

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 
+// you need atleast 2 eventlistner to generate random quotes about motivation, love etc 
+
+// must createElements from within 
+
+// you need queryselector or ALL on h1 or p element
+
+// one form form form validation 
+
+// appendchild remember the node and parents 
+
+// remmeber to use textContent (just like on the lab get it so that when you click onee of the links you will be able to see that word of what they clicked)
+
+// find a way to make style have an affect when they click somewhere on the page ex. dark mode
+
+// include an attribute 
+
+// BOM Property to include maybe do a history, an alert when the form isnt completed fully, need full name birthdate etc
+
+// 
