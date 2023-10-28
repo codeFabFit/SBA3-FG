e053753d8343970b59ffc55f61a32155ad9e0800
codeFabFit <fabgar14@gmail.com>
2023-10-28 11:25:33 -0400

    added new javascript elements and they work no errors

--- main.js
+++ main.js
@@ -2,19 +2,42 @@
 // you need atleast 2 eventlistner to generate random quotes about motivation, love etc 
 const quote =[ "The power is always within you", "It might be tough but your tougher", "If you can do the first 5 you can do the last 5", "Never be afraid to fail, only be afraid to quit"]
 const generateButton = document.getElementById("generateButton");
-const quoteOutput = document.getElementById("quoteOutput");
+// const quoteOutput = document.getElementById("quoteOutput");
 generateButton.addEventListener("click", () => {
-    element.preventDefault();
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex];
+    // Element.preventDefault();
+    const randomIndex = Math.floor(Math.random() * quote.length);
+    const randomQuote = quote[randomIndex];
     quoteOutput.textContent = randomQuote;
+    window.alert(randomQuote);
 });
-console.log(quoteOutput)
+
 // must createElements from within 
 
 // you need queryselector or ALL on h1 or p element
 
 // one form form form validation 
+function validationForm () {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("passwod").value;
+
+    if (!firstName || !lastName || !email || !password) {
+        document.getElementById("error message").textContent = "Must complete all fields to proceed";
+        return false;
+    }
+
+    if (email.indexOf('@') === -1 || email.indexOf(".com") === -1) {
+        document.getElementById("error-message").textContent = "Invalid Email Format";
+        return false;
+    }
+   else {
+    return true;
+
+}
+   } 
+    // window.alert("we recieved your information")
+
 
 // appendchild remember the node and parents 
 
@@ -22,8 +45,20 @@ console.log(quoteOutput)
 
 // find a way to make style have an affect when they click somewhere on the page ex. dark mode
 
+const themeButton = document.getElementById("theme");
+const body = document.body;
+themeButton.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        themeButton.textContent = "Light Mode";
+    } else {
+        themeButton.textContent = "Dark Mode"
+    }
+})
+
+
 // include an attribute 
 
-// BOM Property to include maybe do a history, an alert when the form isnt completed fully, need full name birthdate etc
+// BOM Property to include maybe do a history, 
 
 // 
